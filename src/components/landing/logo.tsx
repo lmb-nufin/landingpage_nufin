6ff0ad238ea4eb3f67433e004d2af7e8b37682bd
Luis Gerardo Martinez Buentello <lmb@nufin.com.mx>
2026-03-19 14:47:46 +0000

    chore: update logo

--- src/components/landing/logo.tsx
+++ src/components/landing/logo.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 export function Logo({ className }: { className?: string }) {
   return (
     <img
-      src="/images/nufinlogo.jpg"
+      src="/images/nufinlogo.png"
       alt="Nufin Logo"
       className={cn("w-auto object-contain", className)}
     />
